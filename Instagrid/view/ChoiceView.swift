93162f53a8ccaa8fff5761bddc2f99b82c3c8e1e
Brian Friess <brian.friesspro@gmail.com>
2021-03-22 11:35:39 +0100

    debut du projet

---
+++ Instagrid/view/ChoiceView.swift
@@ -0,0 +1,22 @@
+//
+//  Layout.swift
+//  Instagrid
+//
+//  Created by Brian Friess on 20/03/2021.
+//
+
+import UIKit
+
+class choiceView : UIView {
+    
+    @IBOutlet var layout : UIImageView!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
