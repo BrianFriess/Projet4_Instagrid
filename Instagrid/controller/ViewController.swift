e4fb3405bcdd0d11c95a48272af37f49f8028fc4
Brian Friess <brian.friesspro@gmail.com>
2021-03-22 11:37:52 +0100

    first commit

--- Instagrid/controller/ViewController.swift
+++ Instagrid/controller/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var choiceView: ChoiceView!
+    
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonViewTwo(buttonTwo)
     }
+    
+    @IBAction func buttonViewOne(_ sender: UIButton) {
+        choiceView.style = .viewOne
+        buttonOne.isSelected = true
+        buttonTwo.isSelected = false
+        buttonThree.isSelected = false
+        buttonOne.setImage(UIImage(named : "Selected"), for: .selected )
+    }
+    
+    @IBAction func buttonViewTwo(_ sender: UIButton) {
+        choiceView.style = .viewTwo
+        buttonOne.isSelected = false
+        buttonTwo.isSelected = true
+        buttonThree.isSelected = false
+        buttonTwo.setImage(UIImage(named: "Selected"), for: .selected)
+    }
+    
+    
+    @IBAction func buttonViewThree(_ sender: UIButton) {
+        choiceView.style = .viewThree
+        buttonOne.isSelected = false
+        buttonTwo.isSelected = false
+        buttonThree.isSelected = true
+        buttonThree.setImage(UIImage(named: "Selected"), for: .selected)
+    }
+    
+    
+    @IBAction func buttonPlusRectangleDown(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    @IBAction func ButtonPlusUpLeft(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    @IBAction func buttonPlusUpRight(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    @IBAction func buttonPlusDownLeft(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    @IBAction func buttonPlusDownRight(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    @IBAction func buttonPlusRectangleUp(_ sender: UIButton) {
+        displayPhotoLibrary()
+    }
+    
+    
+    private func displayPhotoLibrary(){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            self.present(UIImagePickerController(), animated: true, completion: nil)
+        }
+    }
+    
 
-
+    
 }
+
 
