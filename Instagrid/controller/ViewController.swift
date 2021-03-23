5ce717d3e0f9e068a933426dd4673ab43aff3d0f
Brian Friess <brian.friesspro@gmail.com>
2021-03-23 19:13:48 +0100

    ajout StackView

--- Instagrid/controller/ViewController.swift
+++ Instagrid/controller/ViewController.swift
@@ -7,75 +7,118 @@
 
 import UIKit
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController{
 
     
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     @IBOutlet weak var buttonThree: UIButton!
     @IBOutlet weak var choiceView: ChoiceView!
+    var chooseButton : UIButton!
+    
+
     
     
-   override func viewDidLoad() {
-        super.viewDidLoad()
-        buttonViewTwo(buttonTwo)
+
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+         buttonViewTwo(buttonTwo)
+     }
+    
+    
+    enum View{
+        case choiceOne, choiceTwo, choiceThree
     }
     
+    func choiceView(_ choice : View){
+        switch choice{
+        case .choiceOne:
+            choiceView.style = .viewOne
+            buttonOne.isSelected = true
+            buttonTwo.isSelected = false
+            buttonThree.isSelected = false
+            
+        case .choiceTwo:
+            choiceView.style = .viewTwo
+            buttonOne.isSelected = false
+            buttonTwo.isSelected = true
+            buttonThree.isSelected = false
+
+        case .choiceThree:
+            choiceView.style = .viewThree
+            buttonOne.isSelected = false
+            buttonTwo.isSelected = false
+            buttonThree.isSelected = true
+        }
+    }
+    
+    
+    
     @IBAction func buttonViewOne(_ sender: UIButton) {
-        choiceView.style = .viewOne
-        buttonOne.isSelected = true
-        buttonTwo.isSelected = false
-        buttonThree.isSelected = false
-        buttonOne.setImage(UIImage(named : "Selected"), for: .selected )
+        choiceView(.choiceOne)
+        buttonOne.setImage(UIImage(named : "Selected"), for: .selected)
     }
     
     @IBAction func buttonViewTwo(_ sender: UIButton) {
-        choiceView.style = .viewTwo
-        buttonOne.isSelected = false
-        buttonTwo.isSelected = true
-        buttonThree.isSelected = false
+        choiceView(.choiceTwo)
         buttonTwo.setImage(UIImage(named: "Selected"), for: .selected)
     }
     
-    
     @IBAction func buttonViewThree(_ sender: UIButton) {
-        choiceView.style = .viewThree
-        buttonOne.isSelected = false
-        buttonTwo.isSelected = false
-        buttonThree.isSelected = true
+        choiceView(.choiceThree)
         buttonThree.setImage(UIImage(named: "Selected"), for: .selected)
     }
     
     
-    @IBAction func buttonPlusRectangleDown(_ sender: UIButton) {
+    
+    @IBAction func buttonTopLeft(_ sender: UIButton) {
         displayPhotoLibrary()
-    }
-    @IBAction func ButtonPlusUpLeft(_ sender: UIButton) {
-        displayPhotoLibrary()
-    }
-    @IBAction func buttonPlusUpRight(_ sender: UIButton) {
-        displayPhotoLibrary()
-    }
-    @IBAction func buttonPlusDownLeft(_ sender: UIButton) {
-        displayPhotoLibrary()
-    }
-    @IBAction func buttonPlusDownRight(_ sender: UIButton) {
-        displayPhotoLibrary()
-    }
-    @IBAction func buttonPlusRectangleUp(_ sender: UIButton) {
-        displayPhotoLibrary()
+        chooseButton = choiceView.buttonTopLeft
     }
     
+    @IBAction func buttonTopRight(_ sender: UIButton) {
+        displayPhotoLibrary()
+        chooseButton = choiceView.buttonTopRight
+    }
     
-    private func displayPhotoLibrary(){
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-            
-            self.present(UIImagePickerController(), animated: true, completion: nil)
-        }
+    @IBAction func buttonDownLeft(_ sender: UIButton) {
+        displayPhotoLibrary()
+        chooseButton = choiceView.buttonDownLeft
+    }
+    
+    @IBAction func buttonDownRight(_ sender: UIButton) {
+        displayPhotoLibrary()
+        chooseButton = choiceView.buttonDownRight
     }
     
 
+}
+
+
+
+
+
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
+    private func displayPhotoLibrary(){
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            chooseButton.setImage(pickedImage, for: .normal)
+            chooseButton.contentMode = .scaleAspectFill
+            chooseButton.clipsToBounds = true
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 
--- Instagrid/view/ChoiceView.swift
+++ Instagrid/view/ChoiceView.swift
@@ -7,8 +7,40 @@
 
 import UIKit
 
-class ViewOne: UIView {
+class ChoiceView: UIView {
 
-
+    @IBOutlet var buttonTopLeft : UIButton!
+    @IBOutlet var buttonTopRight : UIButton!
+    @IBOutlet var buttonDownLeft : UIButton!
+    @IBOutlet var buttonDownRight : UIButton!
+    
+    
+    
+    
+    enum StyleView{
+          case viewOne
+          case viewTwo
+          case viewThree
+      }
+      
+      var style : StyleView = .viewTwo{
+          didSet{
+              setStyle(style)
+          }
+      }
+    
+    func setStyle(_ style : StyleView){
+        switch style{
+        case .viewOne:
+            buttonTopRight.isHidden = true
+            buttonDownRight.isHidden = false
+        case .viewTwo:
+            buttonTopRight.isHidden = false
+            buttonDownRight.isHidden = true
+        case .viewThree :
+            buttonTopRight.isHidden = false
+            buttonDownRight.isHidden = false
+        }
+    }
 
 }
